8cc50000b9fce4e54266f96b8f3efbe17ed49165
hoggykurnia@gmail.com <hoggykurnia@gmail.com>
2024-05-31 01:44:12 +0700

    dashboard

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GlobalService } from 'src/app/services/global.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+  user: any = {}
+  pendapatanBulanIni: any = [];
+  pendapatanHariIni: any = [];
 
+  constructor(
+    private globalService: GlobalService,
+  ) { }
+
+  ngOnInit(): void {
+    this.user = this.globalService.getAuth().user;
+    this.getPendapatanBulanIni();
+    this.getPendapatanHariIni();
+  }
+
+  empty(){
+    this.pendapatanBulanIni = [];
+    this.pendapatanHariIni = [];
+  }
+
+  getPendapatanBulanIni(){
+    this.globalService.DataGet("/dashboard/pendapatan", {}, false).subscribe((res: any) => {
+      this.pendapatanBulanIni = res.data;
+    }, (error: any) => {
+      this.pendapatanBulanIni = [];
+    })
+  }
+
+  getPendapatanHariIni(){
+    this.globalService.DataGet("/dashboard/penjualanhariini", {}, false).subscribe((res: any) => {
+      this.pendapatanHariIni = res.data;
+    }, (error: any) => {
+      this.pendapatanHariIni = [];
+    })
+  }
 }
